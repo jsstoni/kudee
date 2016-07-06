44761bf8cd1cbf37ba1b45071835113643c5868b
jsstoni <counsantor2d@gmail.com>
2016-07-06 03:13:01 -0430

    lenguage higthligth frontend

--- bin/string.js
+++ bin/string.js
@@ -5,4 +5,36 @@ module.exports = (app) => {
 		var str = str.split(/\n/);
 		return str.join("<br />");
 	}
+	app.locals.language = function(lang) {
+		switch (parseInt(lang)) {
+			case 0: 
+				lang = 'text';
+			break;
+			case 1:
+				lang = 'php';
+			break;
+			case 2:
+				lang = 'markup';
+			break;
+			case 3:
+				lang = 'css';
+			break;
+			case 4:
+				lang = 'python';
+			break;
+			case 5:
+				lang = 'ruby';
+			break;
+			case 6:
+				lang = 'java';
+			break;
+			case 7:
+				lang = 'sql';
+			break;
+			case 8:
+				lang = 'javascript';
+			break;
+		}
+		return lang;
+	}
 }
